82fff2dcc5423be5b9cb5eae3f34d6d95fd8ec7e
Sigmund Berbom <sberbom@gmail.com>
2019-12-13 21:47:57 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header>
+        <img className="profile-pic" alt="profile-pic" src="https://sberbom.s3.eu-north-1.amazonaws.com/_DSC0523+copy-3.jpg"/>
+        <h1>SIGMUND BERBOM</h1>
+        <h2>Student at NTNU</h2>
+        <a href="https://www.linkedin.com/in/sigmund-berbom-80b279158/">
+          <i className="fab fa-linkedin icon"></i>
+        </a>
+        <a href="https://github.com/sberbom">
+          <i className="fab fa-github-square icon"></i>
+        </a>
+        <a href="mailto:sberbom@gmail.com">
+          <i className="fas fa-envelope-square icon"></i>
         </a>
       </header>
     </div>
